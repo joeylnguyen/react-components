0a278312c51568230dc5e99ed75cd07a4994b8f3
Joey Nguyen <joeylnguyen95@gmail.com>
2020-04-06 12:12:20 -0700

    Create grocery list that bolds and changes font size on hover

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
 // TODO
+// import GroceryList from "./GroceryList"
+
+var groceryItems = ['Tapatio', 'Rice', 'Eggs', 'Lentil Chips', 'Tomato', 'Cheese', 'Cereal', 'Dog food', 'Bread'];
+
+
+var App = () => <div><GroceryList groceries={groceryItems}/></div>;
+
+var GroceryList = (props) => {
+  return (
+    <ul>
+      {props.groceries.map(item => <GroceryListItem key={item} groceries={item}/>)}
+    </ul>
+  )
+};
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  onListItemHover() {
+    this.setState({
+      done: !this.state.done
+    });
+    console.log('on it!')
+  }
+
+  onListItemLeave() {
+    this.setState({
+      done: !this.state.done
+    });
+    console.log('left!');
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal',
+      fontSize: this.state.done ? 'x-large' : 'medium'
+    };
+
+    return (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemLeave.bind(this)}>{this.props.groceries}</li>
+    );
+  }
+};
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
+
+
+
+// var Chips = () => <li>Chips</li>;
+// var Corn = () => <li>Corn</li>;
